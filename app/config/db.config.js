8d0900216bc1cdca1d1e97cf63d11832599e7139
Saurabhr24 <110756296+Saurabhr24@users.noreply.github.com>
2023-08-27 14:55:13 +0530

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: "mongodb://0.0.0.0:27017/bezkoder_db"
+  url: "mongodb+srv://saurabhrasse2409:2mBcialp2S8kIeiL@cluster0.lqlherh.mongodb.net/?retryWrites=true&w=majority"
 };
